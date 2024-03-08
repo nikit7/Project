95d7b8ebcb31bd0b2234a413981dcd53f7ceab82
nikit7 <33247721+nikit7@users.noreply.github.com>
2024-03-09 02:16:55 +0800

    feat: Форматирование кода

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-import Sapper as sp
+from Sapper import Sapper as sp
+
 
 root = tk.Tk()
 root.title('Сапёр 8x8')
@@ -7,6 +8,5 @@ root.resizable(width=False, height=False)
 rows = 8
 cols = 8
 mines = 9
-sp.Sapper(root, rows, cols, mines)
+sp(root, rows, cols, mines)
 root.mainloop()
-
